d28d34aa2f941670858cf515a837b6abae2af070
hisa11 <132685722+hisa11@users.noreply.github.com>
2024-02-18 22:32:44 +0900

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,6 @@
 //main.cpp
 #include "mbed.h"
 #include "Servo.hpp"
-#include "Solenoid.hpp"
 
 BufferedSerial pc(USBTX, USBRX, 250000); // パソコンとのシリアル通信
 CAN can1(PA_11, PA_12, (int)1e6);
@@ -9,9 +8,6 @@ CAN can1(PA_11, PA_12, (int)1e6);
 
 int main()
 {
-    SolenoidController solenoidController(can1, pc);
-    solenoidController.run();
-
     ServoController servoController(can1, pc);
     servoController.run();
 }
